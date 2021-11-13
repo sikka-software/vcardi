de7a6dd8cdc06e670f1c68f2fc23187bfd029167
Zakher Masri <zakhermasri@gmail.com>
2021-11-13 03:50:34 +0300

    testing gh pages

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 // const { createVCard } = require("..");
-
+// const DD = require("https://cdn.jsdelivr.net/npm/@sikka/vcardi@1.1.0/index.min.js");
 console.log("we are in site");
-
+console.log("DD ", vCardi); //how can we use the npm package here?
 let fn = document.getElementById("first-name").value;
 console.log("something", fn);
 const form = document.getElementById("maker");
@@ -10,7 +10,7 @@ form.addEventListener("submit", (event) => {
   // stop form submission
 
   event.preventDefault();
-//   console.log(createVCard);
+  //   console.log(createVCard);
   //   for (let i = 0; i < event.target.length - 1; i++) {
   //     console.log("making vcard", event.target[i].value);
   //   }
